8dfba231699f056475d0817e84a905b4b890206e
abdel <abdellah.ha@gmail.com>
2025-02-27 11:53:19 +0000

    commit 27022025

--- myEcomm091024/notification-service/src/main/java/com/programming/techie/notificationservice/NotificationServiceApplication.java
+++ myEcomm091024/notification-service/src/main/java/com/programming/techie/notificationservice/NotificationServiceApplication.java
@@ -2,14 +2,15 @@ package com.programming.techie.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
