78aa275f1bf396cce21332c235247c93f8ff7fab
abdellah2188 <58362022+abdellah2188@users.noreply.github.com>
2025-06-29 23:50:54 +0100

    Add files via upload

--- myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/config/SecurityConfig.java
+++ myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/config/SecurityConfig.java
@@ -1,46 +1,57 @@
-/*
-package com.hamch.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-           .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-      //   .oauth2Login(withDefaults());
-
-        http.cors().disable();
-        http.csrf().disable();
-        return http.build();
-
-    }
-    //@Bean
-    */
-/*@Order(SecurityProperties.DEFAULT_FILTER_ORDER)
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors();
-        http.csrf().disable();
-        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/**","/register/**", "/api/product/**").permitAll();
-        //http.authorizeRequests().antMatchers("/appUser/**","/appRole/**").hasAuthority("ADMIN");
-        http.authorizeRequests().anyRequest().authenticated();
-        System.out.println("security4444444444444444444444444444444");
-
-    }*//*
-
+  package com.hamch.apigatewayservice.config;
+  
+  import java.util.Arrays;
+  
+  import org.springframework.context.annotation.Bean; 
+  import  org.springframework.context.annotation.Configuration; 
+  import  org.springframework.security.config.Customizer; 
+  import  org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; 
+  import  org.springframework.security.config.annotation.web.builders.HttpSecurity;
+  import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity; 
+  import  org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity; 
+  import  org.springframework.security.config.web.server.ServerHttpSecurity ;
+  //import  org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler; 
+  //import  org.springframework.security.oauth2.client.registration.ClientRegistrationRepository; 
+  import  org.springframework.security.web.SecurityFilterChain; 
+  import  org.springframework.web.cors.CorsConfiguration; 
+  import  org.springframework.web.cors.CorsConfigurationSource; 
+  import  org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+  
+  
+  @Configuration
+  
+  @EnableWebSecurity
+  @EnableMethodSecurity(prePostEnabled = true)
+  public class SecurityConfig {
+  
+	  private JwtAuthConverter jwtAuthConverter; 
+	  public  SecurityConfig(JwtAuthConverter jwtAuthConverter) {  this.jwtAuthConverter =  jwtAuthConverter; }
+	  
+	  @Bean 
+	  public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception{ 
+		  return http 
+				  .csrf(Customizer.withDefaults())
+				  .authorizeHttpRequests(ar->ar.requestMatchers("/**").permitAll())
+				  .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+				  .oauth2ResourceServer(o2->o2.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter))) 
+				  .headers(h->h.frameOptions(fo->fo.disable())) 
+				  .cors(cors-> cors.configurationSource(corsConfigurationSource())) 
+				  .build(); 
 }
-*/
+  
+  
+  @Bean 
+  CorsConfigurationSource corsConfigurationSource() { 
+	  CorsConfiguration  configuration = new CorsConfiguration();
+	  //configuration.setAllowedOrigins(Arrays.asList("*"));
+      configuration.applyPermitDefaultValues();
+	  configuration.setAllowedMethods(Arrays.asList("*"));
+	  configuration.setAllowedHeaders(Arrays.asList("*"));
+	  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); 
+	  source.registerCorsConfiguration("/**",  configuration); 
+  return source; 
+  }
+  
+  }
+ 
