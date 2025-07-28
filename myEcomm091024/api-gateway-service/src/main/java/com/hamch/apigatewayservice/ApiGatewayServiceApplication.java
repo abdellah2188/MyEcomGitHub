78dc55510ea688c6f4ecfd746599573c1ceb9675
abdellah2188 <58362022+abdellah2188@users.noreply.github.com>
2025-07-28 23:21:02 +0100

    Add files via upload

--- myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/ApiGatewayServiceApplication.java
+++ myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.hamch.apigatewayservice;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
@@ -11,22 +12,28 @@ import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 //import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 //import org.springframework.context.annotation.Primary;
 //import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+//import lombok.RequiredArgsConstructor;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@SpringBootApplication
+//@SpringBootApplication
 @EnableDiscoveryClient
-//@SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
-//@ComponentScan("com.hamch.apigatewayservice.config")
-//@ComponentScan("com.hamch.apigatewayservice.config.SecurityConfig.class")
-//@Configurable
+@SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
+
+@ComponentScan("com.hamch.apigatewayservice.config")
+@ComponentScan("com.hamch.apigatewayservice.config.SecurityConfig.class")
+@Configurable
+//@RequiredArgsConstructor
 public class ApiGatewayServiceApplication {
     public static void main(String[] args) {
     	
--- myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/config/SecurityConfig.java
+++ myEcomm091024/api-gateway-service/src/main/java/com/hamch/apigatewayservice/config/SecurityConfig.java
@@ -1,49 +1,83 @@
-//package com.hamch.apigatewayservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//
-//import java.util.Arrays;
-//
-//@Configuration
+package com.hamch.apigatewayservice.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+
+@Configuration
 //@EnableWebSecurity
-//@EnableMethodSecurity(prePostEnabled = true)
-//public class SecurityConfig {
-//	
-//    private JwtAuthConverter jwtAuthConverter;
-//
+@EnableWebFluxSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+	
+    //private JwtAuthConverter jwtAuthConverter;
+
 //    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
 //        this.jwtAuthConverter = jwtAuthConverter;
 //    }
-//
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth
+        		.pathMatchers("/**").permitAll()
+        		.anyExchange().authenticated()
+        		)
+    //            .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        http.cors(Customizer.withDefaults());
+        return http.build();
+    }
+   
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //    	
 //    	 System.out.println("TTTTTTTTTTTTTvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
 //        return http
 //                .cors(Customizer.withDefaults())
-//                .authorizeHttpRequests(ar->ar.requestMatchers("/**").permitAll())
-//                //.authorizeHttpRequests(ar->ar.requestMatchers("/api/customer/**").hasAnyAuthority("USER"))
-//         //       .authorizeHttpRequests(ar->ar.requestMatchers("/**").hasAuthority("USER"))
-//               // .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-//                .csrf(csrf->csrf.disable())
+//                .authorizeHttpRequests(ar->ar.requestMatchers( "/**").hasAuthority("USER"))
+////               // .authorizeHttpRequests(ar->ar.requestMatchers("/**").hasAuthority("ADMIN"))
+//                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
 //                .oauth2ResourceServer((ors->ors.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter))))
 //                .headers(h->h.frameOptions(fo->fo.disable()))
-//                //.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"))
+//                .cors(cors -> cors.disable())
+////               // .csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"))
 //                .build();
 //    }
+    
+//    @Bean
+//    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+//    	
+//    	System.out.println("PPPPPRRRRR"+ properties);
+//        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+//    }
 //    
+//    
+    
 //    private static JwtAuthenticationConverter jwtAuthenticationConverter()
 //    {
 //      	 System.out.println("fffffffffffffffffffffffffffff");
@@ -55,20 +89,20 @@
 //        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
 //        return jwtAuthenticationConverter;
 //    }
-//   
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//      	 System.out.println("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
-//
-//        CorsConfiguration configuration = new CorsConfiguration();
-//     //   configuration.addAllowedOrigin(Arrays.asList("http://localhost:4200"));
-//        //configuration.addAllowedOrigin("http://localhost:4200");
-//        configuration.setAllowedMethods(Arrays.asList("*"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//
-//
-//}
+   
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+      	 System.out.println("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        //configuration.addAllowedOrigin("");
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+}
