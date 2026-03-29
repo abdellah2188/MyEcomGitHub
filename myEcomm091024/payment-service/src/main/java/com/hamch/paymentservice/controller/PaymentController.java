dd7802d97818c1077a674d08bf34390af75a7b06
abdellah2188 <abdellah.ha@gmail.com>
2026-03-30 00:07:40 +0100

    Add files via upload

--- myEcomm091024/payment-service/src/main/java/com/hamch/paymentservice/controller/PaymentController.java
+++ myEcomm091024/payment-service/src/main/java/com/hamch/paymentservice/controller/PaymentController.java
@@ -1,20 +1,28 @@
 package com.hamch.paymentservice.controller;
 
-import com.hamch.paymentservice.entities.Payment;
-import com.hamch.paymentservice.repository.PaymentRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hamch.paymentservice.entities.Payment;
+import com.hamch.paymentservice.repository.PaymentRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/payment")
 //@RequiredArgsConstructor
 @Slf4j
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
 public class PaymentController {
     @Autowired
     private PaymentRepository paymentRepository;
